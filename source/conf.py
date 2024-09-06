dc937f617d7f136ed60dd36cdec4a84419619768
ABHAY PRATAP SINGH CHAUHAN <79743613+Abhay240602@users.noreply.github.com>
2024-09-06 17:27:54 +0530

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -15,6 +15,7 @@ release = '1.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
+    'sphinxcontrib.openapi',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',  
